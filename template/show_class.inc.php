4c97f5018ed56c71ed89db0bb54a7c551f7d654d
Karl Vollmer <vollmerk@ampache.org>
2013-02-20 13:34:43 -0400

    - Fix classification not set correctly on record edit

--- template/show_class.inc.php
+++ template/show_class.inc.php
@@ -1,7 +1,8 @@
 <?php 
-/* vim:set tabstop=8 softtabstop=8 shiftwidth=8 noexpandtab: */
+// vim: set softtabstop=2 ts=2 sw=2 expandtab: 
+$class_id = $class_id ? $class_id : $_POST['classification']; 
 ?>
 <?php foreach ($classes as $class) { 
 	$is_selected = ''; 
-	if ($class->uid == $_POST['classification']) { $is_selected = ' selected="selected"'; } 
+	if ($class->uid == $class_id) { $is_selected = ' selected="selected"'; } 
 ?><option value="<?php echo scrub_out($class->uid); ?>"<?php echo $is_selected; ?>><?php echo scrub_out($class->name); ?></option><?php } ?>
