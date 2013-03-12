c19c4f63fded0b509d139e8d10fef1ad11a27333
Karl Vollmer <vollmer@ampache.org>
2013-03-12 17:47:34 -0300

    Only say it worked, if it worked

--- reports.php
+++ reports.php
@@ -14,8 +14,9 @@ switch (\UI\sess::location('action')) {
   case 'request':
     if (Access::has('admin','admin')) { 
       $report = new Report(\UI\sess::location('2'),\UI\sess::location('3')); 
-      $report->request(\UI\sess::location('4')); 
-      Event::add('success','Report scheduled. This may take a long time, you will be e-mailed once the report is complete'); 
+      if ($report->request(\UI\sess::location('4'))) {
+        Event::add('success','Report scheduled. This may take a long time, you will be e-mailed once the report is complete'); 
+      }
     }
     require_once \UI\template('/reports/view'); 
   break; 
