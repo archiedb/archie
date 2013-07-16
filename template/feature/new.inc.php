dfd150193da5388a124ce921cdc60713858fa775
Karl Vollmer <karl.vollmer@gmail.com>
2013-07-16 12:59:50 -0700

    - Creation forms for krotovina and feature

---
+++ template/feature/new.inc.php
@@ -0,0 +1,42 @@
+<?php
+// vim: set softtabstop=2 ts=2 sw=2 expandtab: 
+if (INIT_LOADED != '1') { exit; }
+?>
+<div class="page-header">
+<h4>New Feature Form - <?php echo Config::get('site'); ?></h3>
+</div>
+<?php Event::display('errors'); ?>
+<form class="form-horizontal" id="new_feature" method="post" action="<?php echo Config::get('web_path'); ?>/feature/create">
+<div class="control-group span4<?php Error::display_class('initial_rn'); ?>">
+  <label class="control-label" for="inputInitialRN">Initial RN</label>
+  <div class="controls">
+    <input id="inputInitialRN" name="initial_rn" type="text" value="<?php echo scrub_out($_POST['initial_rn']); ?>" />
+  </div>
+</div>
+<div class="control-group span8<?php Error::display_class('description'); ?>">
+  <label class="control-label" for="inputDescription">How is it defined?</label>
+  <div class="controls">
+    <textarea placeholder="..." rows="4" cols="80" name="description"><?php echo scrub_out($_POST['description']); ?></textarea>
+  </div>
+</div>
+<div class="control-group span4<?php Error::display_class('image'); ?>">
+  <label class="control-label" for="inputUpload">Image</label>
+  <div class="controls">
+    <div class="fileupload fileupload-new" data-provides="fileupload">
+     <div class="input-append">
+      <div class="uneditable-input span3">
+        <i class="icon-file fileupload-exists"></i> <span class="fileupload-preview"></span>
+      </div>
+      <span class="btn btn-file"><span class="fileupload-new">Select</span>
+      <span class="fileupload-exists">Change</span><input name="media" type="file" /></span>
+      <a href="#" class="btn fileupload-exists" data-dismiss="fileupload">Remove</a>
+     </div>
+    </div>
+  </div>
+</div>
+<div class="control-group span8">
+  <div class="controls">
+  	<input type="submit" class="btn btn-primary" value="Create" />
+  </div>
+</div>
+</form>
