d764b51c76fb5c9f81a02317e1fae815c320c8d5
Karl Vollmer <karl.vollmer@gmail.com>
2014-05-07 11:36:21 -0300

    ADD - Northing/Easting/Elevation incase they don't have a total station

--- template/feature/new.inc.php
+++ template/feature/new.inc.php
@@ -7,12 +7,7 @@ if (INIT_LOADED != '1') { exit; }
 </div>
 <?php Event::display('errors'); ?>
 <form class="form-horizontal" id="new_feature" method="post" action="<?php echo Config::get('web_path'); ?>/feature/create">
-<div class="control-group span4<?php Error::display_class('initial_rn'); ?>">
-  <label class="control-label" for="inputInitialRN">Initial RN</label>
-  <div class="controls">
-    <input id="inputInitialRN" name="initial_rn" type="text" value="<?php echo scrub_out($_POST['initial_rn']); ?>" />
-  </div>
-</div>
+
 <div class="control-group span8<?php Error::display_class('description'); ?>">
   <label class="control-label" for="inputDescription">How is it defined?</label>
   <div class="controls">
@@ -23,6 +18,35 @@ if (INIT_LOADED != '1') { exit; }
   <label class="control-label" for="inputKeywords">Keywords</label>
   <div class="controls">
     <input id="inputKeywords" name="keywords" type="text" value="<?php echo scrub_out($_POST['keywords']); ?>" />
+  </div>
+</div>
+<div class="control-group offset1 span10">
+  <h5>Initial RN / Northing, Easting and Elevation</h5>
+  <hr />
+  <i class="muted"><small>Do not enter Northing, Easting and Elevation if using RNs from a Total Station</small></i>
+</div>
+<div class="control-group span4<?php Error::display_class('initial_rn'); ?>">
+  <label class="control-label" for="inputInitialRN">Initial RN</label>
+  <div class="controls">
+    <input id="inputInitialRN" name="initial_rn" type="text" value="<?php echo scrub_out($_POST['initial_rn']); ?>" />
+  </div>
+</div>
+<div class="control-group span4 offset1<?php Error::display_class('northing'); ?>">
+  <label class="control-label" for="inputNorthing">Northing</label>
+  <div class="controls">
+    <input id="inputNorthing" name="northing" type="text" value="<?php echo scrub_out($_POST['northing']); ?>" />
+  </div>
+</div>
+<div class="control-group span4<?php Error::display_class('easting'); ?>"> 
+  <label class="control-label" for="inputEasting">Easting</label>
+  <div class="controls">
+    <input id="inputEasting" name="easting" type="text" value="<?php echo scrub_out($_POST['easting']); ?>" />
+  </div>
+</div>
+<div class="control-grouP span4 offset1<?php Error::display_class('elevation'); ?>">
+  <label class="control-label" for="inputElevation">Elevation</label>
+  <div class="controls">
+    <input id="inputElevation" name="elevation" type="text" value="<?php echo scrub_out($_POST['elevation']); ?>" />
   </div>
 </div>
 <div class="control-group span8">
