2d83f3766623d1e3a636ef8e1761bae44ba19804
Karl Vollmer <karl.vollmer@gmail.com>
2014-06-24 10:45:47 -0300

    REMOVE - Debug

--- class/unit.class.php
+++ class/unit.class.php
@@ -6,7 +6,6 @@ class unit {
 	public $uid; 
 	public $name; 
   public static $values = array();
-	//public static $values = array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'); 
 
 	// Constructor
 	public function __construct($uid) { 
