250b98dd48fae2c9c335b55f2b43beb11fc285fe
Karl Vollmer <karl.vollmer@gmail.com>
2015-02-16 19:53:46 -0400

    UPDATE - Access checkes to use new role/actions

--- records.php
+++ records.php
@@ -6,22 +6,22 @@ require_once 'template/header.inc.php';
 // Switch on the action
 switch (\UI\sess::location('action')) { 
   case 'upload':
-    // Figure out the extension and upload accordingly
+    if (!Access::has('media','create')) { \UI\access_denied(); }
     Content::upload($_POST['record_id'],$_POST,$_FILES,'record'); 
     header('Location:' . Config::get('web_path') . \UI\return_url($_POST['return'])); 
   break;
   case 'image_edit': 
-    if (!Access::has('media','write',$_POST['uid'])) { break; }
+    if (!Access::has('media','edit')) { \UI\access_denied(); }
     Content::update('image',$_POST['uid'],$_POST); 
     header('Location:' . Config::get('web_path') . \UI\return_url($_POST['return'])); 
   break; 
   case '3dmodel_edit':
-    if (!Access::has('media','write',$_POST['uid'])) { break; }
+    if (!Access::has('media','edit')) { \UI\access_denied(); }
     Content::update('3dmodel',$_POST['uid'],$_POST); 
     header('Location:' . Config::get('web_path') . \UI\return_url($_POST['return'])); 
   break;
   case 'image_delete':
-    if (!Access::has('media','delete',$_POST['uid'])) {  break; }
+    if (!Access::has('media','delete')) { \UI\access_denied(); }
     $image = new Content($_POST['uid'],'image'); 
     if (!$image->delete()) { 
       Error::add('delete','Unable to perform image deletion request, please contact administrator'); 
@@ -33,7 +33,7 @@ switch (\UI\sess::location('action')) {
     header('Location:' . Config::get('web_path') . \UI\return_url($_POST['return'])); 
   break; 
   case '3dmodel_delete':
-    if (!Access::has('media','delete',$_POST['uid'])) { break; }
+    if (!Access::has('media','delete')) { \UI\access_denied(); }
     $media = new Content($_POST['uid'],'3dmodel'); 
     if (!$media->delete()) { 
       Event::error('DELETE','Unable to delete media item:' . $media->filename); 
@@ -43,7 +43,7 @@ switch (\UI\sess::location('action')) {
     header('Location:' . Config::get('web_path') . \UI\return_url($_POST['return'])); 
   break;
   case 'media_delete':
-    if (!Access::has('media','delete',$_POST['uid'])) { break; }
+    if (!Access::has('media','delete')) { \UI\access_denied(); }
     $media = new Content($_POST['uid'],'media'); 
     if (!$media->delete()) { 
       Event::error('DELETE','Unable to delete media item:' . $media->filename); 
@@ -53,6 +53,7 @@ switch (\UI\sess::location('action')) {
     header('Location:' . Config::get('web_path') . \UI\return_url($_POST['return'])); 
   break; 
 	case 'update': 
+    if (!Access::has('record','edit')) { \UI\access_denied(); }
 		$record = new Record($_POST['record_id']); 
     // Set to current user  
     $_POST['user'] = \UI\sess::$user->uid;
@@ -66,18 +67,22 @@ switch (\UI\sess::location('action')) {
 	  } 
 	break; 
   case 'edit':
+    if (!Access::has('record','edit')) { \UI\access_denied(); }
 		$record = new Record(\UI\sess::location('objectid')); 
     require_once \UI\template('/records/edit'); 
 	break; 
   case 'view':
+    if (!Access::has('record','read')) { \UI\access_denied(); }
     $record = new Record(\UI\sess::location('objectid')); 
     require_once \UI\template();
   break;
   case 'new':
     Error::clear(); 
+    if (!Access::has('record','create')) { \UI\access_denied(); }
     require_once \UI\template(); 
   break;
   case 'create':
+    if (!Access::has('record','create')) { \UI\access_denied(); }
     $_POST['user'] = \UI\sess::$user->uid;
     if ($record_id = Record::create($_POST)) {
       header('Location:' . Config::get('web_path') . '/records/view/' . scrub_out($record_id));
@@ -88,7 +93,7 @@ switch (\UI\sess::location('action')) {
   break;
   case 'delete': 
     // Admin only
-    if (!Access::has('record','delete',$_POST['record_id'])) {  break; }
+    if (!Access::has('record','delete')) { \UI\access_denied(); }
     // We should do some form ID checking here
     Record::delete($_POST['record_id']);
     header("Location:" . Config::get('web_path') . "/records"); 
@@ -104,6 +109,7 @@ switch (\UI\sess::location('action')) {
     header("Location:" . Config::get('web_path') . '/media/ticket/' . \UI\sess::location('objectid'));
   break; 
   case 'search':
+    if (!Access::has('record','read')) { \UI\access_denied(); }
     // If no post check for get
     if (!isset($_POST['field']) AND !isset($_POST['value'])) { 
       $field = \UI\sess::location('2');
@@ -121,6 +127,7 @@ switch (\UI\sess::location('action')) {
     require_once \UI\template('/records/show'); 
   break;
   case 'sort':
+    if (!Access::has('record','read')) { \UI\access_denied(); }
     $field = \UI\sess::location('objectid') ? \UI\sess::location('objectid') : 'station_index';
     $order = \UI\sess::location('3') ? strtoupper(\UI\sess::location('3')) : '';
     $view = new View(); 
@@ -131,6 +138,7 @@ switch (\UI\sess::location('action')) {
     require_once \UI\template('/records/show'); 
   break; 
   case 'offset': 
+    if (!Access::has('record','read')) { \UI\access_denied(); }
     $view = new View(); 
     $view->set_type('record'); 
     $view->set_start(\UI\sess::location('objectid')); 
@@ -138,6 +146,7 @@ switch (\UI\sess::location('action')) {
     require_once \UI\template('/records/show'); 
   break;
   default:
+    if (!Access::has('record','read')) { \UI\access_denied(); }
     $view = new View(); 
     $view->reset(); 
     $view->set_type('record'); 
