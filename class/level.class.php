4b55e0d1e2976e1c8e9c6c453c0f8d56a21c8a8d
Karl Vollmer <karl.vollmer@gmail.com>
2015-03-10 18:45:26 -0300

    FIX - Adjust access required to close a level is now manager if you aren't an excavator

--- class/level.class.php
+++ class/level.class.php
@@ -571,10 +571,8 @@ class Level extends database_object {
   public function validate_close($input) { 
 
     // Make sure it's an admin or an excavator 
-    if (\UI\sess::$user->access < 100) { 
-//      if (!$this->is_excavator(\UI\sess::$user->uid)) { 
+    if (!$this->is_excavator(\UI\sess::$user->uid) OR Access::has('level','manage')) { 
         Error::add('excavator','Unable to close, you are not a site manager');
-//      }
     }
 
     if (!$this->has_photo()) {
