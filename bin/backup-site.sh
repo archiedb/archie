1d771234268e47a8f9ddae2547bf77c12aab16ec
Karl Vollmer <karl.vollmer@gmail.com>
2015-04-08 10:38:12 -0300

    FIX #46 dropbox isn't really able to be automated easily, but we provide documentation and suggestions now

--- bin/backup-site.sh
+++ bin/backup-site.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # Settings
-BIN="/archie/bin"
-CONTENT="/archie/content"
+ABS_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)/`basename "${BASH_SOURCE[0]}"`
+CONTENT=$ARGV[0]
 DATE=`date +%F`
 
-if [ ! -f ${BIN}/database.auth ] 
+if [ ! -f ${ABS_PATH}/database.auth ] 
 then
- echo "ERROR: Unable to find ${BIN}/database.auth file, please create it and try again"
+ echo "ERROR: Unable to find ${ABS_PATH}/database.auth file, please create it and try again"
  exit 1
 fi
 
-if [ ! -d ${BIN}/backup ]
+if [ ! -d ${ABS_PATH}/backup ]
 then
- echo "ERROR: Directory ${BIN}/backup doesn't exist please create it"
+ echo "ERROR: Directory ${ABS_PATH}/backup doesn't exist please create it"
  exit 1
 fi
 
@@ -22,8 +22,14 @@ then
  exit 1
 fi
 
-mysqldump --defaults-extra-file=${BIN}/database.auth --add-drop-table --allow-keywords archie > ${BIN}/backup/${DATE}.archie.mysql
-tar -C ${BIN}/backup -cjf ${BIN}/backup/${DATE}.archie.mysql.bz2 ${DATE}.archie.mysql
-rm -f ${BIN}/backup/${DATE}.archie.mysql
-tar -C ${CONTENT} -cjf ${BIN}/backup/${DATE}.archie.img.bz2 ${CONTENT}/*
-echo "MySQL Database backed up to ${BIN}/backup/${DATE}.archie.img.bz2"
+echo "Dumping MySQL Database..."
+mysqldump --defaults-extra-file=${ABS_PATH}/database.auth --add-drop-table --allow-keywords archie > ${ABS_PATH}/backup/${DATE}.archie.mysql
+tar -C ${ABS_PATH}/backup -cjf ${ABS_PATH}/backup/${DATE}.archie.mysql.bz2 ${DATE}.archie.mysql
+BCK_SIZE=`stat -c%s ${ABS_PATH}/backup/${DATE}.archie.mysql.bz2`
+echo "MySQL Database backed up to ${ABS_PATH}/backup/${DATE}.archie.mysql.bz2 ${BCK_SIZE} bytes backed up"
+rm -f ${ABS_PATH}/backup/${DATE}.archie.mysql
+echo "Backing up Archie Contact..."
+tar -C ${CONTENT} -cjf ${ABS_PATH}/backup/${DATE}.archie.img.bz2 ${CONTENT}/*
+BCK_SIZE=`stat -c%s ${ABS_PATH}/backup/${DATE}.archie.img.bz2`
+echo "Content backed up to ${ABS_PATH}/backup/${DATE}.archie.img.bz2 ${BCK_SIZE} bytes backed up"
+exit 0
