852a2148ab352c2893f1e4758e63ddfeff759f3a
Karl Vollmer <karl.vollmer@gmail.com>
2015-05-16 11:34:25 -0300

    FIX - fiter on site switch

--- template/site/view.inc.php
+++ template/site/view.inc.php
@@ -2,25 +2,67 @@
 // vim: set softtabstop=2 ts=2 sw=2 expandtab: 
 if (INIT_LOADED != '1') { exit; }
 ?>
+<?php Event::display('errors'); ?>
 <div class="pull-left">
-<h4>Sites</h4>
+  <h4><?php echo scrub_out($site->name); ?> Site</h4>
 </div>
 <p class="pull-right text-right">
-  <a class="btn btn-success" href="<?php echo Config::get('web_path'); ?>/manage/site/add">Add Site</a>
+  <a class="btn btn-primary disabled" href="#addproject">Set Project</a>
+  <a class="btn btn-primary disabled" href="#addaccession">Set Accession</a>
 </p>
-<table class="table table-bordered table-hover">
-<thead>
+<table class="table table-hover table-bordered table-white">
 <tr>
-  <th>Name</th>
-  <th>Description</th>
-  <th>P.I.</th>
-  <th>Records</th>
-  <th>Enabled</th>
-  <th>&nbsp;</th>
+  <td>Name</td>
+  <td>
+    <?php echo scrub_out($site->name); ?>
+  </td>
 </tr>
-</thead>
-<tbody>
-<?php foreach ($sites as $site) { require \UI\template('/site/view_row'); } ?>
-</tbody>
+<tr>
+  <td>
+    Principal Investigator
+  </td>
+  <td>
+      <?php echo scrub_out($site->principal_investigator); ?>
+  </td>
+</tr>
+<tr>
+  <td>
+   Description
+  </td>
+  <td>
+      <?php echo scrub_out($site->description); ?>
+  </td>
+</tr>
+<tr>
+  <td>
+    Partners
+  </td>
+  <td>
+      <?php echo scrub_out($site->partners); ?>
+  </td>
+</tr>
+<tr>
+  <td>
+    Elevation
+  </td>
+  <td>
+      <?php echo scrub_out($site->elevation); ?>
+  </td>
+</tr>
+<tr>
+  <td>
+    Northing
+  </td>
+  <td>
+    <?php echo scrub_out($site->northing); ?>
+  </td>
+</tr>
+<tr>
+  <td>
+    Easting
+  </td>
+  <td>
+    <?php echo scrub_out($site->easting); ?>
+  </td>
+</tr>
 </table>
-</div><!-- End table container --> 
