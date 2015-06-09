feb6bc416d5e8ebb2f1691b3012034de478946b0
Karl Vollmer <karl.vollmer@gmail.com>
2015-06-09 15:07:36 -0300

    Update backup script to include rsyncing and clearing of old files

--- bin/backup-site.sh
+++ bin/backup-site.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # Settings
 ABS_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
-#ABS_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)/`basename "${BASH_SOURCE[0]}"`
 CONTENT=$1
 DATE=`date +%F`
+# Define how many copies to keep
+SAVECOUNT=14
+# Set the target for an RSYNC, if no target is specified it
+# doesn't RSYNC
+#RSYNCTARGET=
 
 if [ ! -f ${ABS_PATH}/database.auth ] 
 then
@@ -34,4 +38,12 @@ echo "Backing up Archie Contact..."
 tar -C ${CONTENT} -cjf ${ABS_PATH}/backup/${DATE}.archie.img.bz2 ${CONTENT}/*
 BCK_SIZE=`stat -c%s ${ABS_PATH}/backup/${DATE}.archie.img.bz2`
 echo "Content backed up to ${ABS_PATH}/backup/${DATE}.archie.img.bz2 ${BCK_SIZE} bytes backed up"
+echo "Clearing Backups older than ${SAVECOUNT} days"
+/usr/bin/find ${ABS_PATH}/backup/ -type f -mtime +${SAVECOUNT} -exec /bin/rm -rvf {} \;
+if [ -n "$RSYNCTARGET"]; then
+  echo "RSYNC Target ${RSYNCTARGET} found, starting RSYNC"
+  rsync -avz --delete-delay ${ABS_PATH}/backup/ ${RSYNCTARGET}
+else
+  echo "No RSYNC Target found, exiting"
+fi
 exit 0
