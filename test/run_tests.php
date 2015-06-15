ddefd593f53cb04371887f04802c50dfaf7a4dfc
Karl Vollmer <karl.vollmer@gmail.com>
2015-06-15 19:39:46 -0300

    FIX - Path for discovery

--- test/run_tests.php
+++ test/run_tests.php
@@ -7,12 +7,10 @@ require_once $prefix . '/class/init.php';
 require_once $prefix . '/test/php/data.php';
 require_once $prefix . '/lib/enhancetest/EnhanceTestFramework.php';
 
-$_SERVER['HTTP_USER_AGENT'] = 'EnhanceTestFramework';
-
 // We need to make it look like a real session
 \UI\sess::set_user(new User(1)); 
 
-\Enhance\Core::discoverTests('./php');
+\Enhance\Core::discoverTests($prefix . '/test/php');
 \Enhance\Core::runTests();
 
 
