9b666f5871b8647d7a212c131d331c8df33ea623
Karl Vollmer <karl.vollmer@gmail.com>
2015-06-15 20:34:17 -0300

    FIX - re-enable no_log

--- test/run_tests.php
+++ test/run_tests.php
@@ -1,7 +1,7 @@
 <?php 
 $file_path = dirname(__FILE__);
 $prefix = realpath($file_path . "/../");
-//define('NO_LOG',1); 
+define('NO_LOG',1); 
 define('CLI',1); 
 require_once $prefix . '/class/init.php'; 
 require_once $prefix . '/test/php/data.php';
