e6130d48e8ddea400c56bd8054ef5bca817b73b9
Karl Vollmer <karl.vollmer@gmail.com>
2015-07-13 16:18:44 -0300

    FIX - installer sql file

--- config/install.sql
+++ config/install.sql
@@ -1,3 +1,3 @@
 LOCK TABLES `users` WRITE;
-INSERT INTO `users` VALUES (1,'admin','admin','root@localhost',100,'8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918',NULL);
+INSERT INTO `users` VALUES (1,'admin','admin','root@localhost','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918','1',NULL,NULL,NULL);
 UNLOCK TABLES;
