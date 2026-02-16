8874de4a3ade17aea957479f229543ccbe4f9b8d
Karl Vollmer <karl.vollmer@gmail.com>
2026-02-16 20:02:33 +0000

    Adjust the structure

--- html/index.php
+++ html/index.php
@@ -1,6 +1,6 @@
 <?php
 // vim: set softtabstop=2 ts=2 sw=2 expandtab: 
-require_once 'class/init.php';
+require_once '../class/init.php';
 
 // The first is "what" we are doing
 switch (\UI\sess::location('page')) { 
